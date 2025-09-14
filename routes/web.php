a0b2ab09138f89f525ac41308c4a9eb52ce33b38
bishnulimbu <subashlimbu987654321@gmail.com>
2025-09-14 22:54:49 +0545

    Refactor routes to include Hero and Objective controllers with authentication middleware

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\HeroController;
 use App\Http\Controllers\HomePageController;
 use App\Http\Controllers\MemberController;
+use App\Http\Controllers\ObjectiveController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,11 +20,14 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::prefix('admin')->name('admin.')->group(function(){
+Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
     Route::resource('/member', MemberController::class);
 });
 
-Route::post('/contact/send',[ContactController::class, 'send'])->name('contact.send');
+Route::post('/contact/send', [ContactController::class, 'send'])->name('contact.send');
+Route::get('/hero/edit',[HeroController::class,'edit'])->name('hero.edit')->middleware('auth');
+Route::put('/hero/update',[HeroController::class, 'update'])->name('hero.update')->middleware('auth');
+Route::resource('/objective', ObjectiveController::class)->middleware('auth');
 
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
