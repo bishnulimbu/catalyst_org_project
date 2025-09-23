c691c9666d308e82344f65ca5a25208a316c8dc4
bishnulimbu <subashlimbu987654321@gmail.com>
2025-09-23 21:11:58 +0545

    updated rotue for forms and controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CesDetailController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HeroController;
@@ -9,7 +10,7 @@ use App\Http\Controllers\ObjectiveController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ScopeController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CesDetailController;
+use App\Http\Controllers\ContactDetailController;
 
 Route::get('/', [HomePageController::class, 'index']);
 
@@ -29,11 +30,15 @@ Route::post('/contact/send', [ContactController::class, 'send'])->name('contact.
 Route::get('/hero/edit', [HeroController::class, 'edit'])->name('hero.edit')->middleware('auth');
 Route::put('/hero/update', [HeroController::class, 'update'])->name('hero.update')->middleware('auth');
 Route::post('/hero', [HeroController::class, 'store'])->name('hero.store')->middleware('auth');
-Route::resource('/scopes',ScopeController::class)->middleware('auth');
+Route::resource('/scopes', ScopeController::class)->middleware('auth');
 Route::resource('objectives', ObjectiveController::class)->middleware('auth');
 
-Route::get('/ces', [CesDetailController::class, 'index'])->name('ces.index');
+Route::get('/ces/form', [CesDetailController::class, 'form'])->name('ces.form');
 Route::get('/ces/{cesDetail}/edit', [CesDetailController::class, 'edit'])->name('ces.edit');
+Route::post('/ces/form', [CesDetailController::class, 'store'])->name('ces.store');
 Route::put('/ces/{cesDetail}', [CesDetailController::class, 'update'])->name('ces.update');
 
+Route::get('/contact/form', [ContactDetailController::class, 'form'])->name('contact.form');
+Route::post('/contact/form', [ContactDetailController::class, 'store'])->name('contact.store');
+Route::put('/contact/{contactDetail}', [ContactDetailController::class, 'update'])->name('contact.update');
 require __DIR__.'/auth.php';
